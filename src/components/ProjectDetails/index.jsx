7ad4a33715cdc6dee30ea8c2421f63e450437106
Rubenhdz20 <rubhernandez42@gmail.com>
2024-06-22 19:09:15 -0600

    Organize project images

--- src/components/ProjectDetails/index.jsx
+++ src/components/ProjectDetails/index.jsx
@@ -16,7 +16,8 @@ function ProjectDetails() {
     const renderImages = () => {
       if (project.images && project.images.length) {
         return project.images.map((image, index) => (
-          <img
+          <img 
+            className='project-details__images'
             key={index}
             src={image} // Assuming image paths are now relative to public folder
             alt={`Image for ${project.name}`}
@@ -30,8 +31,11 @@ function ProjectDetails() {
 
     return (
         <div className='project-details'>
-            <h2>{project.name}</h2>
-            <p>{project.description}</p>
+            <svg className='project-details__svg' width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M18 8C18 8.41421 17.6642 8.75 17.25 8.75H2.56031L8.03063 14.2194C8.32368 14.5124 8.32368 14.9876 8.03063 15.2806C7.73757 15.5737 7.26243 15.5737 6.96937 15.2806L0.219375 8.53063C0.0785422 8.38995 -0.000590086 8.19906 -0.000590086 8C-0.000590086 7.80094 0.0785422 7.61005 0.219375 7.46937L6.96937 0.719375C7.26243 0.426319 7.73757 0.426319 8.03063 0.719375C8.32368 1.01243 8.32368 1.48757 8.03063 1.78062L2.56031 7.25H17.25C17.6642 7.25 18 7.58579 18 8Z" fill="black"/>
+            </svg>
+            <h2 className='project-details__title'>{project.name}</h2>
+            <p className='project-details__description'>{project.description}</p>
             <div className='project-details-images'>{renderImages()}</div>
         </div>
     );
