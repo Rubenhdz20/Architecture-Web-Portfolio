80d0fbc2913336119d5e8bea06873ec2c4a861f4
Rubenhdz20 <rubhernandez42@gmail.com>
2024-06-24 20:01:48 -0600

    Work in scrolling and start to build MobileMenu

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -1,6 +1,14 @@
+import React from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 import './HeroSection.css'
 
 function HeroSection() {
+    const handleClick = () => {
+        scroll.scrollTo('#contact-section');
+    };
+
+    console.log(handleClick);
+
     return(
         <>
             <nav className="hero__nav">
@@ -9,7 +17,7 @@ function HeroSection() {
             <section className="hero__section">
                 <img className="hero__section--background" src='/assets/background 3.jpeg' alt="" />
                 <h1 className="hero__section--first-title">Construyendo el futuro de tu proyecto</h1>
-                <button className="hero__section--button">Empecemos</button>
+                <button className="hero__section--button" onClick={handleClick}>Empecemos</button>
             </section>
         </>
     )
